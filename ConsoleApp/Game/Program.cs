07c9f2c4d2279bd1a07028fc78ea244d327661b2
Bruni <81496681+BruniHH@users.noreply.github.com>
2024-11-18 13:58:10 +0300

    Update Program.cs

--- ConsoleApp/Game/Program.cs
+++ ConsoleApp/Game/Program.cs
@@ -4,15 +4,14 @@ using Players;
 namespace Game;
 public class Program{
     public static List<Player> AllPlayers{ get;set; } = new();
-
     private static void Main(string[] args){
         Console.Clear();
         Commands c = new();
-		while(true){
-            Console.WriteLine("Привет! Введи комманду -->");
-            string a = Console.ReadLine();
-            c.WhatCmd(a);
-            if (a.StartsWith("/exit")) Environment.Exit(0);
+	while(true){
+        	Console.WriteLine("Привет! Введи комманду -->");
+        	string a = Console.ReadLine();
+        	c.WhatCmd(a);
+        	if (a.StartsWith("/exit")) Environment.Exit(0);
         }
 
     }
